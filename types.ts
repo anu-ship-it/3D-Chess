e19f05389f30dc9e31e4edb80248c3fa1fade04d
anu-ship-it <anup17508@gmail.com>
2025-12-26 23:21:16 +0530

    Last changes of the day

--- types.ts
+++ types.ts
@@ -1,32 +1,32 @@
 import { Square, PieceSymbol, Color } from 'chess.js';
 
 export interface BoardProps {
-    fen: string;
-    onMove: (from: Square, to: Square) => void;
-    validMoves: Square[];
-    selectedSquare: Square | null;
-    onselectedSquare: (square: Square | null) => void;
-    lastMove: { from: Square; to: Square } | null;
+  fen: string;
+  onMove: (from: Square, to: Square) => void;
+  validMoves: Square[];
+  selectedSquare: Square | null;
+  onSelectSquare: (square: Square | null) => void;
+  lastMove: { from: Square; to: Square } | null;
 }
 
 export interface PieceProps {
-    type: PieceSymbol;
-    color: Color;
-    position: [number, number, number];
-    square: Square;
-    onClick: (e: any) => void;
-    isSelected: boolean;   
+  type: PieceSymbol;
+  color: Color;
+  position: [number, number, number];
+  square: Square;
+  onClick: (e: any) => void;
+  isSelected: boolean;
 }
 
 export type MoveData = {
-    from: Square;
-    to: Square;
-    promotion?: string;
+  from: Square;
+  to: Square;
+  promotion?: string;
 };
 
 export enum GameStatus {
-    PLAYING = 'playing',
-    CHECKMATE = 'checkmate',
-    DRAW = 'draw',
-    STALEMATE = 'stalemate',
+  PLAYING = 'playing',
+  CHECKMATE = 'checkmate',
+  DRAW = 'draw',
+  STALEMATE = 'stalemate',
 }
