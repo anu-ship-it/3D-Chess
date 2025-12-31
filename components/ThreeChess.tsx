336ee2b8b511e17d37ee67144a73da9a0da1f35b
anu-ship-it <anup17508@gmail.com>
2026-01-01 00:02:55 +0530

    More to come

--- components/ThreeChess.tsx
+++ components/ThreeChess.tsx
@@ -1,3 +1,4 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Canvas, useThree } from '@react-three/fiber';
-import 
+import { OrbitControls, Environment, ContactShadows, Text, MeshReflectorMaterial, Float, Stars } from '@react-three/drei';
+import { Square, Color, PieceSymbol } from 'chess.js';
