f03d5aaa5f51c8c986f0b74d25f2eee1d02e5611
anu-ship-it <anup17508@gmail.com>
2026-01-02 15:53:46 +0530

    Need more changes

--- components/ThreeChess.tsx
+++ components/ThreeChess.tsx
@@ -2,4 +2,8 @@ import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls, Environment, ContactShadows, Text, MeshReflectorMaterial, Float, Stars } from '@react-three/drei';
 import { Square, Color, PieceSymbol } from 'chess.js';
-import
+import * as Pieces from './ChessPieces';
+import { useSpring, animated } from '@react-spring/three';
+import * as THREE from 'three';
+
+// --- Constrants ---
